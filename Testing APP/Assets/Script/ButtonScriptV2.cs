341deb5aa5fae49b246158f0e64afb3e9ab417fa
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2021-09-30 22:46:48 +0800

    Just need to export outas an app

---
+++ Testing APP/Assets/Script/ButtonScriptV2.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScriptV2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoToCorridor()
+    {
+        SceneManager.LoadScene("View360PIC");
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("ARscene");
+    }
+}
