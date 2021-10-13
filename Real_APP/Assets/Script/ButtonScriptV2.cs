aa330301e1263a86b6d06ff79e7c2ce9ed4bb335
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2021-10-13 15:20:45 +0800

    NewApp

---
+++ Real_APP/Assets/Script/ButtonScriptV2.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScriptV2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoTo360View()
+    {
+        SceneManager.LoadScene("View360PIC");
+    }
+
+    public void GoToCorridor()
+    {
+        SceneManager.LoadScene("Hallway");
+    }
+
+    public void GoTo360Video()
+    {
+        SceneManager.LoadScene("View360Video");
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("ARscene");
+    }
+}
---
+++ Real_APP/Assets/Script/Pan_Zoom_Script.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pan_Zoom_Script : MonoBehaviour
+{
+    public static Pan_Zoom_Script Instance;
+
+   Vector3 touchStart;
+    public float zoomOutMin;
+    public float zoomOutMax;
+    public float m_FieldOfView;
+
+    public GameObject VideoUI;
+    public GameObject PhotoUI;
+
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        VideoUI.SetActive(false);
+        PhotoUI.SetActive(false);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        if (Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+            float difference = currentMagnitude - prevMagnitude;
+
+            zoom(difference * 20f);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Camera.main.transform.position += direction;
+        }
+        zoom(Input.GetAxis("Mouse ScrollWheel"));
+
+        if(Camera.main.fieldOfView == 50)
+        {
+            VideoUI.SetActive(true);
+            PhotoUI.SetActive(true);
+        }
+
+        if (Camera.main.fieldOfView >= 75)
+        {
+            VideoUI.SetActive(false);
+            PhotoUI.SetActive(false);
+        }
+    }
+
+    void zoom(float increment)
+    {
+        Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView - increment, zoomOutMin, zoomOutMax);
+    }
+
+}
