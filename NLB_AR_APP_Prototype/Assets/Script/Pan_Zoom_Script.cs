11b5ec06c1ad4e081d47e1908e412a6e6716d5b4
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2021-11-09 18:07:10 +0800

    Need to ask DR Aung

---
+++ NLB_AR_APP_Prototype/Assets/Script/Pan_Zoom_Script.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pan_Zoom_Script : MonoBehaviour
+{
+    public static Pan_Zoom_Script Instance;
+
+    Vector3 touchStart;
+    public float zoomOutMin;
+    public float zoomOutMax;
+
+    public GameObject VideoUI;
+    public GameObject PhotoUI;
+
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        if (Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+            float difference = currentMagnitude - prevMagnitude;
+
+            zoom(difference * 20f);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Camera.main.transform.position += direction;
+        }
+        zoom(Input.GetAxis("Mouse ScrollWheel"));
+
+    }
+
+    void zoom(float increment)
+    {
+        Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView - increment, zoomOutMin, zoomOutMax);
+    }
+}
---
+++ NLB_AR_APP_Prototype/Assets/Script/Rotate_Script.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Rotate_Script : MonoBehaviour
+{
+    public float rotationRate = 3.0f;
+    private float m_previousX;
+    private float m_previousY;
+    private LayerMask targetLayer;
+
+    //to keep track if we are dragging or not
+    public bool isDragging;
+
+    //to keep track if is it rotating
+    public bool isRotating;
+
+    //the model that we want it to drag
+    public GameObject AnimalModel;
+
+    public Animator cubeAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AnimalModel = this.gameObject.GetComponent<GameObject>();
+        cubeAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this is for moble control
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+
+            if (Physics.Raycast(ray, out hit, 1000, targetLayer))
+            {
+                isRotating = true;
+                m_previousX = Input.GetTouch(0).position.x;
+                m_previousY = Input.GetTouch(0).position.y;
+            }
+
+
+        }
+
+        // get the user touch input
+        if (Input.GetMouseButton(0))
+        {
+            var touch = Input.mousePosition;
+            var deltaX = -(Input.mousePosition.y - m_previousY) * rotationRate;
+            var deltaY = -(Input.mousePosition.x - m_previousX) * rotationRate;
+            transform.Rotate(deltaX, deltaY, 0, Space.World);
+
+            m_previousX = Input.mousePosition.x;
+            m_previousY = Input.mousePosition.y;
+        }
+
+    }
+
+}
