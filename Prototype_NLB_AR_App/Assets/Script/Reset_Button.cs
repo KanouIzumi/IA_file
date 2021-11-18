0bd877beabfc08e4a166f3e3e7728fbbb819c890
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2021-11-18 18:28:07 +0800

    k

---
+++ Prototype_NLB_AR_App/Assets/Script/Reset_Button.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Reset_Button : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public GameObject resetGameObject;
+    bool position = false;
+    public Vector3 StartPosition;
+    public Quaternion StartRotation;
+    public Vector3 StartScale;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (position == false)
+            return;
+
+        resetGameObject.transform.position = StartPosition;
+        resetGameObject.transform.rotation = StartRotation;
+        resetGameObject.transform.localScale = StartScale;
+
+    }
+
+
+    public void OnPointerDown(PointerEventData pointerEventData)
+    {
+        position = true;
+    }
+
+    public void OnPointerUp(PointerEventData pointerEventData)
+    {
+        position = false;
+    }
+
+}
